0f9afad9b18c2ef1bd4294c1836f2c68252d3b1a
Syed Noman Shariq <Nshariq562@gmail.com>
2023-08-24 05:17:27 +0500

    first python class done

---
+++ welcome.py
@@ -0,0 +1,12 @@
+line1 = '********************' # Header and Footer
+line2 = '*                  *' # Side Content
+line3 = '*     WELCOME      *' # Main Content
+
+# main code starts with here and start with black print
+
+print()
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)                   

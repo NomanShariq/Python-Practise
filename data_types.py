f9746b8bcfabdfaedbb65daa6663fc693526312c
Syed Noman Shariq <Nshariq562@gmail.com>
2023-08-25 06:35:03 +0500

    Second Class done

---
+++ data_types.py
@@ -0,0 +1,104 @@
+
+## Literal Assignment 
+
+fName = 'Noman'
+lName = 'Shariq'
+
+print()
+# print(type(fName))
+# print(type(fName) == str)
+# print(isinstance(fName,str))
+
+# print(fName)
+# print(fName.upper())
+# print(fName.lower())
+
+## Constructor Function 
+
+# Pizza = str("Pepperoni")
+# print(type(Pizza))
+# print(type(Pizza) == str)
+# print(isinstance(Pizza,str))
+
+## Concatenation
+
+# fullname = fName + ' ' + lName
+# print(fullname)
+
+# fullname += '!!!!!!!!!!!'
+# print(fullname) 
+
+## Casting a integer to string
+
+# Year = str(2000)
+# print(type(Year))
+# print('I am listening this band since ' + Year + 's')
+
+## Multi-line Strings
+
+multiLines = '''
+
+Hello !!!!!!!!!!!!!!!!! 
+
+My Name Is NOMAN SHARIQ 
+                                            WHAT ABOUT YOU??
+'''
+
+# print(multiLines)
+# print(multiLines.replace('WHAT ABOUT YOU','I Am Good'))
+# print(multiLines)
+
+# print(len(multiLines))
+# multiLines += "                                                                                                                   "
+# print(len(multiLines))
+# multiLines = "               " + multiLines
+# print(len(multiLines))
+
+# print(len(multiLines.strip()))
+# print(len(multiLines.lstrip()))
+# print(len(multiLines.rstrip()))
+# print(len(multiLines))
+
+## Escaping Special Characters
+
+# Sentence = 'I\'m Back At work\tWhat\'s the ending \\time \nbro??'
+# print(Sentence)
+
+## Building Menu
+
+# title = " Menu ".upper()
+# print(title.center(20,"="))
+# print("Burger".ljust(20,"-") + "$4".rjust(5))
+# print("Pizza".ljust(20,"-") + "$3".rjust(5))
+# print("Fries".ljust(20,"-") + "$2".rjust(5))
+
+## String Indexes
+
+# print(fName[1])
+# print(fName[-1])
+# print(fName[1:-1])
+# print(fName[1:])
+
+## Some method may return boolean data
+
+# print(fName.startswith("N"))
+# print(fName.endswith("No"))
+
+## Boolean Data types
+# myValue = True 
+# x = bool(False)
+# print(type(myValue))
+# print(isinstance(myValue,bool))
+
+## Integer Data type 
+
+# price = 399
+# best_price = int(100)
+# print(type(price))
+# print(isinstance(best_price,int))
+
+## Float  Data Type
+
+gpa = 3.54
+y = float(1.21)
+print(type(gpa))
---
+++ if_else.py
@@ -0,0 +1,11 @@
+number = 8 
+print()
+
+# ----------  if else operators -------------- #
+# if number > 49 :
+#     print('Number is greater')
+# else : 
+#     print('NUmber is lesser')
+
+# ----------  ternary operators operators -------------- #
+# print('Number is greater') if number > 30 else print('Number is lesser') 

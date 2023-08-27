eb84dd74ef5e6175bd6816a2b43ffab47c0a673f
Syed Noman Shariq <Nshariq562@gmail.com>
2023-08-28 02:38:41 +0500

    Loops work , Rock,paper,Scissor game done with loops and fifith clas done

---
+++ loops.py
@@ -0,0 +1,70 @@
+value = 1
+
+# basic while loop
+# while value <= 10 :
+#     print(value)
+#     value += 1
+    
+# Little modified
+
+# while value <= 10 :
+#     print(value)
+
+#     if value == 5 :
+#         break
+#     value += 1
+
+# While Loop with continue method
+
+# while value <= 10 :
+#         value += 1
+
+#         if value == 5 :
+#             continue
+#         print(value)
+        
+# else : print('The value of loop is ' + str(value))
+
+
+# For loops 
+
+names = ["Noman","Ahmed","Shariq"]
+
+# for x in names :
+#     print(x)
+
+# for x in "Pineabbple" :
+#      print(x)
+
+# for x in names:
+#     if x == "Ahmed":
+#         break
+#     print(x)
+
+# for x in names :
+#     if x == "Ahmed" :
+#         continue
+#     print(x)
+
+# for x in range(4):
+#     print(x)
+
+# for x in range(0,5):
+#     print(x)
+
+# for x in range(0,100,5):
+#     print(x)
+
+# for x in range(5,101,5):
+#     print(x)
+# else : print('Glad it\'s over!')
+
+actions = ["eats","play","sleeps"]
+
+# for name in names:
+#     for action in actions:
+#         print(name  + " " + action)
+
+# for action in actions:
+#     for name in names:
+#         print(name  + " " + action)
--- rps_game.py
+++ rps_game.py
@@ -6,29 +6,44 @@ class RPS(Enum):
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
-
-playerChoice = input("Enter Your Value\n 1. Rock \n 2. Paper \n 3.Scissors\n\n")
-player = int(playerChoice)
-
-if player > 1 | player < 3 :
-    sys.exit("Please Enter the 1 , 2 , 3")
     
-computerChoice = random.choice("123")
-computer = int(computerChoice)
-
-print("")
-print("Player choice is " + str(RPS(player)).replace("RPS.","") + ".")
-print("Player choice is " + str(RPS(computer)).replace("RPS.","") + ".")
-print("")
-
-if player == 1 and computer == 3:
-    print(" 🎉 Player Wins!!")
-elif player == 2 and computer == 1:
-    print(" 🎉 Player Wins!!")
-elif player == 3 and computer == 2:
-    print(" 🎉 Player Wins!!")
-elif player ==  computer:
-    print(" 😲 Tie!! ")
-else :
-    print(" 🐍 Python Wins !!")
+Playagain = True
     
+while Playagain:
+    
+        playerChoice = input("Enter Your Value\n 1. Rock \n 2. Paper \n 3.Scissors\n\n")
+        player = int(playerChoice)
+
+        if player > 1 | player < 3 :
+            sys.exit("Please Enter the 1 , 2 , 3")
+            
+        computerChoice = random.choice("123")
+        computer = int(computerChoice)
+
+        print("")
+        print("You choice" +" "+ playerChoice + ".")
+        print("Python choice"+" " + computerChoice + ".")
+        # print("Player choice is " + str(RPS(player)).replace("RPS.","") + ".")
+        # print("Python choice is " + str(RPS(computer)).replace("RPS.","") + ".")
+        print("")
+
+        if player == 1 and computer == 3:
+            print(" 🎉 Player Wins!!")
+        elif player == 2 and computer == 1:
+            print(" 🎉 Player Wins!!")
+        elif player == 3 and computer == 2:
+            print(" 🎉 Player Wins!!")
+        elif player ==  computer:
+            print(" 😲 Tie!! ")
+        else :
+            print(" 🐍 Python Wins !!")
+
+        Playagain = input("\nPlay Again?\n Y for Yes \n Q for Quit\n\n")
+        
+        if Playagain.lower() == "y":
+            continue
+        else : print("\n🎉🎉🎉🎉")
+        print('Thank you playing\n')
+        Playagain = False
+        
+sys.exit("👋🏼 Bye")
